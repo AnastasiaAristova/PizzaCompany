ad7abe7d506156ba3c4dcc35d206e7cd12830b0b
AnastasiaAristova <86532105+AnastasiaAristova@users.noreply.github.com>
2021-10-01 21:40:45 +0300

    Delete pizzaApp directory

--- pizzaApp/pizzaCompany/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-from .models import Pizza
-
-
-def index(request):
-    pizzas = Pizza.objects.all()
-    return render(request, "pizzaCompany/index.html", {"pizzas": pizzas})
